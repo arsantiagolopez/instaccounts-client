b33b9701279b72edcda9471eaffbf506ce694f2c
Alex Santiago Lopez <alexsantiagolopez@Alexs-MacBook-Pro.local>
2021-11-16 21:49:42 -0600

    TypeScript Boilerplate finished with NextAuth fixes.

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -1,27 +1,30 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
 
-const uri: string = process.env.MONGODB_URI || "";
-const options: any = {
+const uri: string = process.env.DATABASE_URL || "";
+const options: MongoClientOptions = {
+  //@ts-ignore
   useUnifiedTopology: true,
   useNewUrlParser: true,
 };
 
-let global: any | undefined;
 let client: MongoClient;
-let clientPromise: any;
+let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
+if (!process.env.DATABASE_URL) {
   throw new Error("Please add your Mongo URI to .env");
 }
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  //@ts-ignore
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
+    //@ts-ignore
     global._mongoClientPromise = client.connect();
   }
+  //@ts-ignore
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
